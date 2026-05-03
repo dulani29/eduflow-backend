ab62eb0f313f28941d468562efee1ce33beacef1
IT24102890 <it24102890@my.sliit.lk>
2026-05-03 19:07:04 +0530

    updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Rate limiting
 const limiter = rateLimit({
